c83fbad5977efc7e9318f4add9637823f169e585
yurysukhorukov <30953718+yurysukhorukov@users.noreply.github.com>
2019-09-19 22:05:10 +0300

    This is 3 commits in 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
-package PACKAGE_NAME;
-
 public class Main {
-
+  public static void main(String[] args) {
+    SomeClass.someMethod();
+    for (int i = 0; i < 100; i++) {
+      System.err.println("Hello world!");
+    }
+  }
 }
---
+++ src/SomeClass.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class SomeClass {
+
+  public static void someMethod() {
+    Scanner scanner = new Scanner(System.in);
+    String words = scanner.nextLine();
+    System.out.println(words);
+  }
+}
